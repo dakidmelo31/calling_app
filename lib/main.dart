8a55bb48b4161435e127a3acbe64d65708357c75
Ndoye Philip Ndula <ndoyephilipndula@gmail.com>
2022-10-07 14:40:38 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calling/screens/groupCall/group_call_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "SF UI",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GroupCallScreen(),
+    );
+  }
+}
